b6fbaf57718a73db06452d3105a270ce83a88622
Elad Ohana <69722689+eohana-unc@users.noreply.github.com>
2022-03-16 23:05:49 -0400

    Wrote testing framework for server session

--- testing/test.js
+++ testing/test.js
@@ -21,25 +21,49 @@ function write_tests(tests) {
 }
 
 function write_server_session(test){
-  let port = 1000 + Math.floor(Math.random() * (2**16 - 1000) )
-  result += `# TEST: ${test["description"]}\n`;
-  result += `echo "Running session for \${BLUE}${test["test-name"]}\${NC}"\n`;
-  result += `(${test["run-command"]} ${join_args(test["args"])}) & \n`;
+
+  let string_replacement = {
+    "%PORT%": 1024 + Math.floor(Math.random() * (49151 - 1024) )
+  };
+  const server_command = `${test["run-command"]} ${join_args(test["args"])}`
+  // Add description in file for easy locating
+  let result = `# TEST: ${test["description"]}\n`;
+  // Display the test being run
+  result += `echo "Running session for \${BLUE}${test["test-name"]}\${NC}\\n"\n`;
+  // run the command with the args (will replace port at the end)
+  result += `(${test["run-command"]} ${join_args(test["args"])}) > tmpfiles/server_output & sleep 1`; 
   for (let ct of test["client-tests"]){
-    result += write_client_test(ct);
+    result += `${write_client_test(ct)}\n\n`;
   }
-  result += `\n\n`
+
+  if (test["server-persistent"]){
+    result += `ps | grep "node ../server.js --port=%PORT%" | grep -v grep | awk '{print $1}' | read pid\n`;
+    result += `kill $pid\n`;
+  }
+  result += `expected=$(echo "${test["expected-output"]}")\n`;
+  result += `result=$(cat tmpfiles/server_output)\n`;
+  result += `match="$(echo $result | grep -E $expected)"\n`;
+  result += `echo "Expected: $expected"\n`;
+  result += `echo "Result: $result"\n`;
+  result += `echo "Match: $match"\n`;
+  result += `[ -n "$match" ] && echo "\${GREEN}Passed ${test["test-name"]}\${NC}" || echo "\${RED}Failed ${test["test-name"]}\${NC}"`;
+  result += `\n\n`;
+  for (let item in string_replacement){
+    // console.log(`${item}: ${string_replacement[item]}`);
+    result = result.replaceAll(item, string_replacement[item]);
+  }
+  return result;
 }
 
 function write_client_test(test){
-  return "\n"
+  return "\n";
 }
 function join_args(args){
   let result = "";
-  for (let arg in args){
-    result += `$arg `
+  for (let arg of args){
+    result += `${arg} `;
   }
-  return result
+  return result;
 }
 // for (let test of tests){
 //   console.log(test);
@@ -51,7 +75,7 @@ function join_args(args){
 let content = `#!/bin/zsh\n\n`;
 content += `RED='\\e[1;31m'\n`;
 content += `BLUE='\\e[1;34m'\n`;
-content += `GREEN='\\e[1;34m'\n`;
+content += `GREEN='\\e[1;32m'\n`;
 content += `NC='\\e[0m'\n`;
 content += write_tests(tests)
 // content += `echo "hello world"`;
--- testing/test.sh
+++ testing/test.sh
@@ -2,9 +2,22 @@
 
 RED='\e[1;31m'
 BLUE='\e[1;34m'
-GREEN='\e[1;34m'
+GREEN='\e[1;32m'
 NC='\e[0m'
 # TEST: Run normal server operations
-echo "Running session for ${BLUE}Normal server on random port${NC}"
+echo "Running session for ${BLUE}Normal server on random port${NC}\n"
+(node ../server.js --port=38231 ) > tmpfiles/server_output & sleep 1
+
+
+ps | grep "node ../server.js --port=38231" | grep -v grep | awk '{print $1}' | read pid
+kill $pid
+expected=$(echo "App listening on port 38231")
+result=$(cat tmpfiles/server_output)
+match="$(echo $result | grep -E $expected)"
+echo "Expected: $expected"
+echo "Result: $result"
+echo "Match: $match"
+[ -n "$match" ] && echo "${GREEN}Passed Normal server on random port${NC}" || echo "${RED}Failed Normal server on random port${NC}"
+
 
 
