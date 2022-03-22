182fc9553ee3382d789401aa52cf653db4d2d6f3
Elad Ohana <69722689+eohana-unc@users.noreply.github.com>
2022-03-21 22:50:45 -0400

    Updated testing framework to make generic tests and added more client tests for normal operations

--- testing/test.js
+++ testing/test.js
@@ -4,106 +4,126 @@ const fs = require("fs");
 const f = fs.readFileSync("./tests.json");
 const tests = JSON.parse(f);
 
+const quotes = {
+  "%q%": "\"",
+  "%Q%": "\\\""
+}
 
 function write_tests(tests) {
   let result = ``;
-  // loop through tests
-
+  // loop through tests and write individual ones
   for (let test of tests){
-    result += write_server_session(test)
+    result += write_test(test)
     result += `\n\n`
   }
   return result;
 }
 
-function write_server_session(test){
-
-  let string_replacement = {
-    "%PORT%": 1024 + Math.floor(Math.random() * (49151 - 1024) )
-  };
-  const server_command=`${test["run-command"]}${join_args(test["args"])}`
-  // Add description in file for easy locating
+function write_test(test){
+  // write test description
   let result = `# TEST: ${test["description"]}\n`;
-  // Display the test being run
+  // echo test name to user
   result += `echo "Running session for \${BLUE}${test["test-name"]}\${NC}\\n"\n`;
-  // run the command with the args (will replace port at the end)
-  result += `(${server_command}) > tmpfiles/server_output & sleep 1\n\n`; 
-  for (let ct of test["client-tests"]){
-    result += `${write_client_test(ct)}\n\n`;
+
+  // run starting command if it exists
+  if (test["run-command"]){
+    const runCommand = test["fork-command"] ? `(${test["run-command"]}) ${test["fork-command"]}` : `${test["run-command"]}`;
+    result += `${runCommand}\n`;
   }
 
-  if (test["server-persistent"]){
-    result += `ps | grep "${server_command}" | grep -v grep | awk '{print $1}' | read pid\n`;
-    result += `kill $pid\n`;
+  // if this is a session with multiple tests, write the sub-tests
+  if (test["sub-tests"]){
+    result += `# Writing subtests\n`;
+    result += `\n`;
+    for (let subtest of test["sub-tests"]){
+      result += `${write_test(subtest)}\n`;
+    }
+    result += '\n';
+    result += `# Finished subtests\n`;
+
   }
-  result += `result=$(cat tmpfiles/server_output)\n`;
-  result += write_evaluation(test)
-  result += `\n\n`;
-  for (let item in string_replacement){
-    result = result.replaceAll(item, string_replacement[item]);
+
+  // kill session if needed
+  if (test["kill-command"]){
+    result += `${test["kill-command"]}\n`;
   }
-  return result;
+
+  // evaluate results
+  result += write_evaluation(test);
+
+  // replace %ARGS%
+  const args = join_args(test["args"]);
+  result = result.replaceAll("%ARGS%", args);
+
+  // replace other miscellaneous %stuff%
+  for (let item in test["dict"]){
+    result = result.replaceAll(item, write_misc(test["dict"][item]));
+  }
+
+  // replace port number
+  // if (test["port"] === "random"){
+  //   const port = 1024 + Math.floor(Math.random() * (49151 - 1024));
+  //   result = result.replaceAll("%PORT%", port);
+  // }
+  for (let quote in quotes) {
+    result = result.replaceAll(quote, quotes[quote]);
+  }
+  return `${result}\n`;
 }
 
-function write_client_test(test){
-  let result = ``;
-  let string_replacement = {
-    "%Q%": "\\\""
-  };
-  result += `# Client test for ${test["description"]}\n`;
-  result += `echo "Running client test for ${test["test-name"]}"\n`;
-  result += `result=$(curl${join_args(test["args"])} ${test["address"]})\n`;
-  result += write_evaluation(test)
-  result += `\n`;
-  for (let item in string_replacement){
-    result = result.replaceAll(item, string_replacement[item]);
-  }
-  return result;
-}
 
 function write_evaluation(test){
-  let result = "";
+  let result = `result=\$(${test["test-command"]})\n`;
   result += `expected=$(echo "${test["expected"]}\\n")\n`;
   result += `match="$(echo $result | grep -E $expected)"\n`;
-  // result += `echo "Expected: $expected"\n`;
-  // result += `echo "Result: $result"\n`;
-  // result += `echo "Match: $match"\n`;
   result += `[ -n "$match" ] && ( echo "\${GREEN}Passed ${test["test-name"]}\${NC}" && echo "\${GREEN}${test["test-name"]}\${NC}" >> tmpfiles/passed ) || ( echo "\${RED}Failed ${test["test-name"]}\${NC}" &&  echo "\${RED}${test["test-name"]}\${NC}" >> tmpfiles/failed &&
   echo "Expected: $expected" >> tmpfiles/failed && echo "Result: $result" >> tmpfiles/failed ) `;
-  // result += `[ -n "$match" ] && ( echo "\${GREEN}Passed ${test["test-name"]}\${NC}" ) || ( echo "\${RED}Failed ${test["test-name"]}\${NC}" )`;
-  // result += `[ -n "$match" ] && ( echo "\${GREEN}Passed ${test["test-name"]}\${NC}" && passed=\`expr $passed + 1\` ) || ( echo "\${RED}Failed ${test["test-name"]}\${NC}" && failed=\`expr $failed + 1\`)`;
   return result
 }
 function join_args(args){
-  let result = "";
-  for (let arg of args){
-    result += ` ${arg}`;
-  }
-  return result;
-}
-// for (let test of tests){
-//   console.log(test);
-// }
-// TODO:
 
+  return args.join(" ");
+}
+
+function write_misc(replacementString){
+  const items = replacementString.split("-*-");
+  if (items[1] === "random"){
+    const lower = parseInt(items[2]);
+    const upper = parseInt(items[3]);
+    const randomNumber = parseInt(lower + Math.floor(Math.random() * (upper - lower)));
+    replacementString = randomNumber.toString();
+  }
+  return replacementString
+}
 // write to test.sh
 
+// declare zsh
 let content = `#!/bin/zsh\n\n`;
+
+// declare color variables
 content += `RED='\\e[1;31m'\n`;
 content += `BLUE='\\e[1;34m'\n`;
 content += `GREEN='\\e[1;32m'\n`;
 content += `NC='\\e[0m'\n`;
 
-content += `echo "" > tmpfiles/passed; echo "" > tmpfiles/failed;\n\n`
-// content += `declare -i passed; declare -i failed; declare -i total\n\n`
+// write new passed and failed test files
+content += `echo "Passed Tests:" > tmpfiles/passed; echo "Failed Tests:" > tmpfiles/failed;\n\n`
+
+// write the tests
 content += write_tests(tests)
 
-// content += `echo "Total tests: $total, Passed \${GREEN}$passed\${NC}, Failed \${RED}$failed\${NC}\\n"\n`
-// content += `echo "hello world"`;
-content += `echo "Passed tests:" && cat tmpfiles/passed\n`
-content += `echo "\\n"\n`
-content += `echo "Failed tests:" && cat tmpfiles/failed\n`
+// create some space after tests
+content += `echo "\\n\\n"\n`;
 
+// display the successful tests
+content += `cat tmpfiles/passed\n`;
+content += `echo "\\n"\n`;
+
+// display the failed tests:
+content += `cat tmpfiles/failed\n`;
+content += `echo "\\n"\n`;
+
+// write to file test.sh
 fs.writeFile("test.sh", content, err => {
   if (err){
     console.error(err);
--- testing/test.sh
+++ testing/test.sh
@@ -4,58 +4,91 @@ RED='\e[1;31m'
 BLUE='\e[1;34m'
 GREEN='\e[1;32m'
 NC='\e[0m'
-echo "" > tmpfiles/passed; echo "" > tmpfiles/failed;
+echo "Passed Tests:" > tmpfiles/passed; echo "Failed Tests:" > tmpfiles/failed;
 
 # TEST: Run normal server operations
 echo "Running session for ${BLUE}Normal server on random port${NC}\n"
-(node ../server.js --port=9379) > tmpfiles/server_output & sleep 1
+(node ../server.js --port=27312 > tmpfiles/server_output) & sleep 1
 
-# Client test for Run curl on app/flip
-echo "Running client test for app/flip"
-result=$(curl http://localhost:9379/app/flip)
+# Writing subtests
+
+# TEST: Run curl on app/flip
+echo "Running session for ${BLUE}app/flip${NC}\n"
+result=$(curl http://localhost:27312/app/flip)
 expected=$(echo "{['\"]?flip['\"]?:['\"]?(heads|tails)['\"]?}\n")
 match="$(echo $result | grep -E $expected)"
 [ -n "$match" ] && ( echo "${GREEN}Passed app/flip${NC}" && echo "${GREEN}app/flip${NC}" >> tmpfiles/passed ) || ( echo "${RED}Failed app/flip${NC}" &&  echo "${RED}app/flip${NC}" >> tmpfiles/failed &&
   echo "Expected: $expected" >> tmpfiles/failed && echo "Result: $result" >> tmpfiles/failed ) 
 
-
-# Client test for Get header on app/flip
-echo "Running client test for app/flip header"
-result=$(curl -I http://localhost:9379/app/flip)
+# TEST: Get header on app/flip
+echo "Running session for ${BLUE}app/flip header${NC}\n"
+result=$(curl -I http://localhost:27312/app/flip)
 expected=$(echo "200 OK\n")
 match="$(echo $result | grep -E $expected)"
 [ -n "$match" ] && ( echo "${GREEN}Passed app/flip header${NC}" && echo "${GREEN}app/flip header${NC}" >> tmpfiles/passed ) || ( echo "${RED}Failed app/flip header${NC}" &&  echo "${RED}app/flip header${NC}" >> tmpfiles/failed &&
   echo "Expected: $expected" >> tmpfiles/failed && echo "Result: $result" >> tmpfiles/failed ) 
 
-
-# Client test for Get not found header
-echo "Running client test for not found header"
-result=$(curl -I http://localhost:9379/app/invalid)
+# TEST: Get not found header
+echo "Running session for ${BLUE}not found header${NC}\n"
+result=$(curl -I http://localhost:27312/app/invalid)
 expected=$(echo "404 [Nn][Oo].*[Nn][Dd]\n")
 match="$(echo $result | grep -E $expected)"
 [ -n "$match" ] && ( echo "${GREEN}Passed not found header${NC}" && echo "${GREEN}not found header${NC}" >> tmpfiles/passed ) || ( echo "${RED}Failed not found header${NC}" &&  echo "${RED}not found header${NC}" >> tmpfiles/failed &&
   echo "Expected: $expected" >> tmpfiles/failed && echo "Result: $result" >> tmpfiles/failed ) 
 
-
-# Client test for Get not found
-echo "Running client test for not found"
-result=$(curl http://localhost:9379/app/invalid)
+# TEST: Get not found
+echo "Running session for ${BLUE}not found${NC}\n"
+result=$(curl http://localhost:27312/app/invalid)
 expected=$(echo "404 [Nn][Oo].*[Nn][Dd]\n")
 match="$(echo $result | grep -E $expected)"
 [ -n "$match" ] && ( echo "${GREEN}Passed not found${NC}" && echo "${GREEN}not found${NC}" >> tmpfiles/passed ) || ( echo "${RED}Failed not found${NC}" &&  echo "${RED}not found${NC}" >> tmpfiles/failed &&
   echo "Expected: $expected" >> tmpfiles/failed && echo "Result: $result" >> tmpfiles/failed ) 
 
+# TEST: Random flips
+echo "Running session for ${BLUE}Random flips${NC}\n"
+result=$(curl http://localhost:27312/app/flips/129)
+expected=$(echo "{['\"]?raw['\"]?:\s*\[((['\"]?tails['\"]?|['\"]?heads['\"]?),?){129}\],\s*['\"]?summary['\"]?:{(['\"]?tails['\"]?:\d{1,6},?|['\"]heads['\"]:\d{1,6},?){1,2}}}\n")
+match="$(echo $result | grep -E $expected)"
+[ -n "$match" ] && ( echo "${GREEN}Passed Random flips${NC}" && echo "${GREEN}Random flips${NC}" >> tmpfiles/passed ) || ( echo "${RED}Failed Random flips${NC}" &&  echo "${RED}Random flips${NC}" >> tmpfiles/failed &&
+  echo "Expected: $expected" >> tmpfiles/failed && echo "Result: $result" >> tmpfiles/failed ) 
 
-ps | grep "node ../server.js --port=9379" | grep -v grep | awk '{print $1}' | read pid
-kill $pid
+# TEST: Call Heads
+echo "Running session for ${BLUE}Call heads${NC}\n"
+result=$(curl http://localhost:27312/app/flip/call/heads)
+expected=$(echo "(win|lose)\n")
+match="$(echo $result | grep -E $expected)"
+[ -n "$match" ] && ( echo "${GREEN}Passed Call heads${NC}" && echo "${GREEN}Call heads${NC}" >> tmpfiles/passed ) || ( echo "${RED}Failed Call heads${NC}" &&  echo "${RED}Call heads${NC}" >> tmpfiles/failed &&
+  echo "Expected: $expected" >> tmpfiles/failed && echo "Result: $result" >> tmpfiles/failed ) 
+
+# TEST: Call Tails
+echo "Running session for ${BLUE}Call tails${NC}\n"
+result=$(curl http://localhost:27312/app/flip/call/tails)
+expected=$(echo "(win|lose)\n")
+match="$(echo $result | grep -E $expected)"
+[ -n "$match" ] && ( echo "${GREEN}Passed Call tails${NC}" && echo "${GREEN}Call tails${NC}" >> tmpfiles/passed ) || ( echo "${RED}Failed Call tails${NC}" &&  echo "${RED}Call tails${NC}" >> tmpfiles/failed &&
+  echo "Expected: $expected" >> tmpfiles/failed && echo "Result: $result" >> tmpfiles/failed ) 
+
+# TEST: Call invalid
+echo "Running session for ${BLUE}Call invalid${NC}\n"
+result=$(curl http://localhost:27312/app/flip/call/invalid)
+expected=$(echo "404 [Nn][Oo].*[Nn][Dd]\n")
+match="$(echo $result | grep -E $expected)"
+[ -n "$match" ] && ( echo "${GREEN}Passed Call invalid${NC}" && echo "${GREEN}Call invalid${NC}" >> tmpfiles/passed ) || ( echo "${RED}Failed Call invalid${NC}" &&  echo "${RED}Call invalid${NC}" >> tmpfiles/failed &&
+  echo "Expected: $expected" >> tmpfiles/failed && echo "Result: $result" >> tmpfiles/failed ) 
+
+
+# Finished subtests
+ps | grep "node ../server.js --port=27312" | grep -v grep | awk '{print $1}' | read pid
+; kill $pid
 result=$(cat tmpfiles/server_output)
-expected=$(echo "App listening on port 9379\n")
+expected=$(echo "App listening on port 27312\n")
 match="$(echo $result | grep -E $expected)"
 [ -n "$match" ] && ( echo "${GREEN}Passed Normal server on random port${NC}" && echo "${GREEN}Normal server on random port${NC}" >> tmpfiles/passed ) || ( echo "${RED}Failed Normal server on random port${NC}" &&  echo "${RED}Normal server on random port${NC}" >> tmpfiles/failed &&
   echo "Expected: $expected" >> tmpfiles/failed && echo "Result: $result" >> tmpfiles/failed ) 
 
 
-
-echo "Passed tests:" && cat tmpfiles/passed
+echo "\n\n"
+cat tmpfiles/passed
 echo "\n"
-echo "Failed tests:" && cat tmpfiles/failed
+cat tmpfiles/failed
+echo "\n"
