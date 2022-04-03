ed77a336b8960e391765cbcba7361c3fac3c9ebb
Elad Ohana <69722689+eohana-unc@users.noreply.github.com>
2022-04-03 17:16:03 -0400

    Implemented database functionality

--- database.js
+++ database.js
@@ -1,9 +1,57 @@
-const Database = require("better-sqlite3")
-const getpath = require("./getpath.js")
+"use strict";
 
-const db = new Database(`${getpath(process.argv[1])}/databases/log.db`)
-// what does this do?
+const Database = require("better-sqlite3");
+const getpath = require("./getpath.js");
 
+const db = new Database(`${getpath(process.argv[1])}/databases/log.db`);
+
+const tableName = db.prepare(`SELECT 
+  name
+FROM 
+  sqlite_master
+WHERE 
+  type ='table' AND 
+  name = 'log';`).get();
+
+if (!tableName){
+  const dbInit = `CREATE TABLE log (
+    id INTEGER PRIMARY KEY,
+    remoteaddr TEXT,
+    remoteuser TEXT,
+    time INTEGER,
+    method TEXT,
+    url TEXT,
+    protocol TEXT,
+    httpversion TEXT,
+    secure TEXT,
+    status INTEGER,
+    referer TEXT,
+    useragent TEXT
+    );`;
+  db.exec(dbInit);
+}
+
+// const dbInsert = (`INSERT INTO log 
+// (remoteaddr, remoteuser, time, 
+//   method, url, protocol, 
+//   httpversion, secure, status, 
+//   referer, useragent) 
+// VALUES
+//   (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
+// `);
+// const stmt = db.prepare(dbInsert)
+// stmt.run("a", "b", 234, "h", "u", "p", "v", "ns", "s", "r", "u")
+
+// const row = db.prepare("SELECT * FROM log");
+// // console.log(row.run());
+// console.log(row.all())
+
+
+// 2022-12-12
+// if (!table) {
+//   console.log("No table")
+// }
+// console.dir(db)
 // so it needs to create a database if there isn't one already
 // it needs to add the data types
 // it needs to provide the database to the export
@@ -23,4 +71,4 @@ const db = new Database(`${getpath(process.argv[1])}/databases/log.db`)
 //   useragent: req.headers['user-agent']
 // }
 
-module.exports=db
+module.exports=db;
--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express')
+// const morgan = require("morgan")
+
 const fs = require("fs")
-const app = express()
 const args = require("minimist")(process.argv)
 const getpath = require("./getpath.js")
 const db = require("./database.js")
-// console.log(db)
+const coin = require("./modules/coin.js")
+
+const app = express()
+const port = args["port"] || 5000
 
 if (args["help"]){
   const text = fs.readFileSync(`${getpath(process.argv[1])}/files/help.txt`, {encoding: "ascii", flag:"r"})
@@ -12,11 +16,71 @@ if (args["help"]){
   process.exit(0)
 }
 
-const coin = require("./modules/coin.js")
 
-const port = args["port"] || 5000
+// const db = require('./database.js')
 
+
+
+// morgan(function (tokens, req, res) {
+//   return [
+//     tokens.method(req, res),
+//     tokens.url(req, res),
+//     tokens.status(req, res),
+//     tokens.res(req, res, 'content-length'), '-',
+//     tokens['response-time'](req, res), 'ms'
+//   ].join(' ')
+// })
 // console.log(coin.coinFlip())
+
+// app.use(morgan(':date :method :url :status :res[content-length] - :response-time ms'))
+app.use((req, res, next) => {
+  next()
+
+  let logdata = {
+    remoteaddr: req.ip || "",
+    remoteuser: req.user || "",
+    time: Date.now(),
+    method: req.method || "",
+    url: req.url || "",
+    protocol: req.protocol || "",
+    httpversion: req.httpVersion || "",
+    secure: req.secure || "",
+    status: res.statusCode || "",
+    referer: req.headers['referer'] || "",
+    useragent: req.headers['user-agent'] || ""
+}
+
+// console.log(logdata)
+const dbInsert = (`INSERT INTO log 
+    (remoteaddr, remoteuser, time, 
+      method, url, protocol, 
+      httpversion, secure, status, 
+      referer, useragent) 
+  VALUES
+    (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+  `)
+  const stmt = db.prepare(dbInsert)
+  stmt.run(logdata.remoteaddr, logdata.remoteuser, logdata.time, logdata.method,
+    logdata.url, logdata.protocol, logdata.httpversion, logdata.secure, logdata.status,
+    logdata.referer, logdata.useragent)
+
+// console.dir(morgan)
+//   const row = db.prepare("SELECT * FROM log WHERE time = ?")
+//  console.log(row.get(logdata.time))
+})
+
+
+// if (args["debug"]){
+//   // console.log("debug mode")
+//   app.get('/app/log/access', (req, res) => {
+//     const stmt = db.prepare("SELECT * FROM log")
+//     const data = stmt.all()
+//     res.statusCode = 200
+//     res.statusMessage = "OK"
+//     res.json(data).end()
+//   })
+// }
+
 
 app.get('/app', (req, res) => {
   res.statusCode = 200
